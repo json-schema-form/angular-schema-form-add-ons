d0bdf3db0116a0cbc6c2a1b31e036d80a6f6d18f
Nicklas Börjesson <nicklas@optimalbpm.se>
2015-06-24 13:15:33 +0200

    Improve naming and add special README

--- tools/create_new_add_on.py
+++ tools/create_new_add_on.py
@@ -32,14 +32,45 @@ if os.path.exists(_destination):
 
 _source_folder = os.path.join(script_dir, "..", "examples", "minimal")
 
+
+
 # Copy to destination
 shutil.copytree(src=_source_folder, dst=_destination,
-                ignore=ignore_patterns('bower_components', 'node_modules'))
+                ignore=ignore_patterns('bower_components', 'node_modules', "README.md"))
 
-# Replace all the Minimal mentions
-mass_replace(_destination, "Minimal", _add_on_name.capitalize() )
-# Replace all the minimal mentions
-mass_replace(_destination, "minimal", _add_on_name.lower())
+# Add README
+shutil.copy(src=os.path.join(script_dir, "README_new_add_on.md"), dst=os.path.join(_destination,"README.md"))
+
+# Create needed variations of the add-on name:
+
+# First, the add-on name should only be word-separated by "-".
+_add_on_name = _add_on_name.replace(" ", "-").replace("_", "-")
+
+# JS names should be camelCase:d and contain no "-"
+_cc_tmp = ''.join(x for x in _add_on_name.title() if x.isalpha())
+_camelCased_add_on_name = (_cc_tmp[0].lower() + _cc_tmp[1:]).replace("-", "")
+
+# References cannot contain any "-"
+_references_add_on_name = _add_on_name.replace("_", "").replace("-", "")
+
+# Actually replace the strings in all the files
+
+# Replace all the capitalized Minimal mentions
+mass_replace(_destination, "Minimal", _add_on_name.capitalize())
+
+# Replace all the minimalForm type references minimal mentions with camelCase:d _add_on_name
+mass_replace(_destination, "minimalForm", _camelCased_add_on_name + "Form")
+
+# Replace all the html file references with lowercase _add_on_name
+mass_replace(_destination, "angular-schema-form-minimal", "angular-schema-form-" + _add_on_name.lower())
+
+# Replace all the destination_folders references with lowercase _add_on_name
+mass_replace(_destination, "destination_folder", _add_on_name.lower())
+
+
+# Replace all the remaining lowercase(references) minimal mentions
+mass_replace(_destination, "minimal", _references_add_on_name.lower())
+
 # Replace all the Optimal BPM mentions
 mass_replace(_destination, "Optimal BPM", _add_on_author)
 
